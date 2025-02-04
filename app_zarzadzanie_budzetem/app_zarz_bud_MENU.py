86ba29028f30ed4c9abd14c004f2f91c1c5aa5c6
JakZaj93 <kub93zajac@gmail.com>
2025-02-04 18:51:17 +0100

    Menu do aplikacji zarządzania budżetem

---
+++ app_zarzadzanie_budzetem/app_zarz_bud_MENU.py
@@ -0,0 +1,36 @@
+def menu():
+    #lista_akcji = ListaZakupow()
+
+    while True:
+        print("\nMenu: \n"
+              "1. Dodanie przychodów do budżetu\n"
+              "2. Odejmowanie wydatków od budżetu\n"
+              "3. Dodanie stałych/cyklicznych wydatków\n"
+              "4. Export raportu di pliku\n"
+              "5. Przeliczenie Walut\n"
+              "6. Historia Wydatków\n"
+              "0. Wyjście z Aplikacji")
+        wybor = input("Wybierz opcję: ")
+
+
+        if wybor == "1":
+            print("tu trzeba zrobić kod do dodawania budzetu do użytkownika")
+        elif wybor == "2":
+            print("tu trzeba zrobić kod do odejmowania wydatków od budżetu użytkownika")
+        elif wybor == "3":
+            print("Tu trzeba zrobić funkcję która będzie cyklicznie odejmować wydatki od budżetu użytkownika ")
+        elif wybor == "4":
+            print("Tu trzeba zrobić funkcje exportowania do pliku raportu z budżetem ")
+        elif wybor == "5":
+            print("Tu trzeba zrobić funkcję do przeliczania walut")
+        elif wybor == "6":
+            print("Tu trzeba zrobić funkcję do zapisywania historii wydatków")
+        elif wybor == "0":
+            print("Koniec planowanie/wydawania/zarządzania \n"
+                  "***Do ZoBaCzEnIA***")
+            break
+        else:
+            print("BŁĄD.... Spróbuj ponownie....")
+
+if __name__ == "__main__":
+    menu()
