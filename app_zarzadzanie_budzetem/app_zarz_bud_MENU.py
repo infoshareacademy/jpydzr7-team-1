b02e8a376ae74286d8d510924d86c48e0136fdda
JakZaj93 <kub93zajac@gmail.com>
2025-02-27 00:07:20 +0100

    poprawiony plik MENU

--- app_zarzadzanie_budzetem/app_zarz_bud_MENU.py
+++ app_zarzadzanie_budzetem/app_zarz_bud_MENU.py
@@ -4,28 +4,29 @@ def menu():
     while True:
         print("\nMenu: \n"
               "1. Dodanie przychodów do budżetu\n"
-              "2. Odejmowanie wydatków od budżetu\n"
-              "3. Dodanie stałych/cyklicznych wydatków\n"
+              "2. Dodaj wydatki do budżetu\n"
+              "3. Dodaj stałe wydatki lub dochody\n"
               "4. Export raportu di pliku\n"
               "5. Przeliczenie Walut\n"
               "6. Historia Wydatków\n"
+              "7. Zaplanuj budżet\n"
               "0. Wyjście z Aplikacji")
-        wybor = input("Wybierz opcję: ")
+        choice = input("Wybierz opcję: ")
 
 
-        if wybor == "1":
-            print("tu trzeba zrobić kod do dodawania budzetu do użytkownika")
-        elif wybor == "2":
+        if choice == "1":
+            print("tu trzeba zrobić kod do dodawania budżetu do użytkownika")
+        elif choice == "2":
             print("tu trzeba zrobić kod do odejmowania wydatków od budżetu użytkownika")
-        elif wybor == "3":
+        elif choice == "3":
             print("Tu trzeba zrobić funkcję która będzie cyklicznie odejmować wydatki od budżetu użytkownika ")
-        elif wybor == "4":
+        elif choice == "4":
             print("Tu trzeba zrobić funkcje exportowania do pliku raportu z budżetem ")
-        elif wybor == "5":
+        elif choice == "5":
             print("Tu trzeba zrobić funkcję do przeliczania walut")
-        elif wybor == "6":
+        elif choice == "6":
             print("Tu trzeba zrobić funkcję do zapisywania historii wydatków")
-        elif wybor == "0":
+        elif choice == "0":
             print("Koniec planowanie/wydawania/zarządzania \n"
                   "***Do ZoBaCzEnIA***")
             break
