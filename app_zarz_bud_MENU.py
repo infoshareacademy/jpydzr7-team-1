0b74fa970bb84e81fcd4a98df269c7855c42609a
Ajabl <jablonski.adr@gmail.com>
2025-03-02 00:47:18 +0100

    budget - dodanie inputu oraz typu platnosci - miesieczna czy jednorazowa

--- app_zarz_bud_MENU.py
+++ app_zarz_bud_MENU.py
@@ -1,3 +1,10 @@
+import uuid
+from email.policy import default
+
+import budget_input
+
+user_ID = uuid.UUID("47a74fbc-d4a7-4bce-ab6b-851c0420592d")
+
 def menu():
     #lista_akcji = ListaZakupow()
 
@@ -5,28 +12,28 @@ def menu():
         print("\nMenu: \n"
               "1. Dodanie przychodów do budżetu\n"
               "2. Dodaj wydatki do budżetu\n"
-              "3. Dodaj stałe wydatki lub dochody\n"
-              "4. Export raportu di pliku\n"
-              "5. Przeliczenie Walut\n"
-              "6. Historia Wydatków\n"
-              "7. Zaplanuj budżet\n"
+              # "3. Dodaj stałe wydatki lub dochody\n"
+              "3. Export raportu do pliku\n"
+              "4. Przeliczenie Walut\n"
+              "5. Historia Wydatków\n"
+              "6. Zaplanuj budżet\n"
               "0. Wyjście z Aplikacji")
         choice = input("Wybierz opcję: ")
 
 
         if choice == "1":
-            print("tu trzeba zrobić kod do dodawania budżetu do użytkownika")
+            budget_input.take_input("income", user_ID)
         elif choice == "2":
-            print("tu trzeba zrobić kod do odejmowania wydatków od budżetu użytkownika")
+            budget_input.take_input("expense", user_ID)
+        # elif choice == "3":
+        #     print("Tu trzeba zrobić funkcję która będzie cyklicznie odejmować wydatki od budżetu użytkownika ")
         elif choice == "3":
-            print("Tu trzeba zrobić funkcję która będzie cyklicznie odejmować wydatki od budżetu użytkownika ")
-        elif choice == "4":
             print("Tu trzeba zrobić funkcje exportowania do pliku raportu z budżetem ")
-        elif choice == "5":
+        elif choice == "4":
             print("Tu trzeba zrobić funkcję do przeliczania walut")
-        elif choice == "6":
+        elif choice == "5":
             print("Tu trzeba zrobić funkcję do zapisywania historii wydatków")
-        elif choice == "0":
+        elif choice == "6":
             print("Koniec planowanie/wydawania/zarządzania \n"
                   "***Do ZoBaCzEnIA***")
             break
